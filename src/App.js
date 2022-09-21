7bb96c1b9e03548d3ddec36cfb9c2ea1ea0c83f5
milan <pperisic.milan@gmail.com>
2022-09-21 19:55:28 +0200

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   const title = 'Welcome to the new blog';
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>{ title }</h1>
       </div>
