9ea79dca4831b83dc8bac2b211a2bf35803f558c
milan <pperisic.milan@gmail.com>
2022-09-22 14:04:34 +0200

    Listing blogs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Home = () => {
-  
+
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
@@ -10,7 +10,12 @@ const Home = () => {
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      { blogs.map((blog) => (
+        <div className="blog-preview" key={ blog.id }>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 }
