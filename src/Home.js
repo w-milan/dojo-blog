71aaf217b10cb5cea138aa2691e69e22f7e8554e
milan <pperisic.milan@gmail.com>
2022-09-22 13:51:36 +0200

    Added blogs

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,16 @@
 import { useState } from "react";
 
 const Home = () => {
-
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName('luigi');
-    setAge(28);
-  }
+  
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={ handleClick }>Click me</button>
     </div>
   );
 }
